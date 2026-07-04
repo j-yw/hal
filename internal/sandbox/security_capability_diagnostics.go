f1d23872c01e11a8753bf2cde0c8e6d9699a6b56
j-yw <jiaoyiwei@gmail.com>
2026-07-04 09:00:46 +0800

    test: US-001 verify secure-default diagnostics

--- internal/sandbox/security_capability_diagnostics.go
+++ internal/sandbox/security_capability_diagnostics.go
@@ -128,6 +128,7 @@ func sandboxSecurityCapabilityReadinessMissingDiagnosticSummary() SandboxSecurit
 		Classification:       SandboxSecurityCapabilityDiagnosticClassificationReadinessMissing,
 		AdvisoryOnly:         true,
 		WouldBlockStrictGate: true,
+		ReasonCode:           SandboxSecurityCapabilityReasonReadinessMissing,
 	}
 	return SandboxSecurityCapabilityReadinessDiagnosticSummary{
 		Status:               SandboxSecurityCapabilityDiagnosticSummaryStatusUnknown,
