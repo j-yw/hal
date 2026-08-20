fcdd8f464062780c36d4bad03aa6dc5f4d45dd60
v <jiaoyiwei@gmail.com>
2026-08-20 19:36:58 +0800

    fix: reject L8 D3 special-use destinations

--- internal/credentialproxy/destination.go
+++ internal/credentialproxy/destination.go
@@ -20,8 +20,9 @@ func resolveAzureResponsesTarget(ctx context.Context, resolver AzureResponsesRes
 	selected := netip.Addr{}
 	for _, address := range addresses {
 		address = address.Unmap()
-		if unsafeAzureResponsesAddress(address) || seen[address] {
-			if unsafeAzureResponsesAddress(address) {
+		unsafe := unsafeAzureResponsesAddress(address)
+		if unsafe || seen[address] {
+			if unsafe {
 				return "", ErrRouteUpstreamUnavailable
 			}
 			continue
@@ -69,16 +70,36 @@ func translatedAzureResponsesAddress(address netip.Addr) (netip.Addr, bool) {
 
 var azureResponsesSpecialUsePrefixes = []netip.Prefix{
 	netip.MustParsePrefix("0.0.0.0/8"),
+	netip.MustParsePrefix("10.0.0.0/8"),
 	netip.MustParsePrefix("100.64.0.0/10"),
+	netip.MustParsePrefix("127.0.0.0/8"),
 	netip.MustParsePrefix("169.254.0.0/16"),
+	netip.MustParsePrefix("172.16.0.0/12"),
 	netip.MustParsePrefix("192.0.0.0/24"),
 	netip.MustParsePrefix("192.0.2.0/24"),
+	netip.MustParsePrefix("192.31.196.0/24"),
+	netip.MustParsePrefix("192.52.193.0/24"),
+	netip.MustParsePrefix("192.88.99.0/24"),
+	netip.MustParsePrefix("192.168.0.0/16"),
+	netip.MustParsePrefix("192.175.48.0/24"),
 	netip.MustParsePrefix("198.18.0.0/15"),
 	netip.MustParsePrefix("198.51.100.0/24"),
 	netip.MustParsePrefix("203.0.113.0/24"),
+	netip.MustParsePrefix("224.0.0.0/4"),
 	netip.MustParsePrefix("240.0.0.0/4"),
+	netip.MustParsePrefix("::/96"),
+	netip.MustParsePrefix("::ffff:0:0/96"),
+	netip.MustParsePrefix("::ffff:0:0:0/96"),
 	netip.MustParsePrefix("100::/64"),
-	netip.MustParsePrefix("2001:2::/48"),
+	netip.MustParsePrefix("100:0:0:1::/64"),
+	netip.MustParsePrefix("2001::/23"),
 	netip.MustParsePrefix("2001:db8::/32"),
+	netip.MustParsePrefix("2002::/16"),
+	netip.MustParsePrefix("2620:4f:8000::/48"),
+	netip.MustParsePrefix("3fff::/20"),
+	netip.MustParsePrefix("5f00::/16"),
+	netip.MustParsePrefix("fc00::/7"),
+	netip.MustParsePrefix("fe80::/10"),
 	netip.MustParsePrefix("fec0::/10"),
+	netip.MustParsePrefix("ff00::/8"),
 }
